b47773838401d73a8be9dfff0019a2311f25d46d
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-03-13 15:19:48 +1100

    Updated Script

---
+++ practice/day7/challenge1/main.py
@@ -0,0 +1,19 @@
+#Step 1 
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random
+random_word = random.choice(word_list)
+print(random_word)
+
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input('Can you guess a word: ').lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for letter in random_word:
+  if letter == guess:
+    print("Right")
+  else:
+    print("Wrong")
