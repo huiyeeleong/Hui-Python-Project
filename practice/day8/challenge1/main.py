0a29b19f2143f9036430774d8285b3a169c66ac2
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-03-13 20:52:42 +1100

    Updated New Script

---
+++ practice/day8/challenge1/main.py
@@ -0,0 +1,30 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# def greet():
+#   print("Hello")
+#   print("How do you do")
+#   print("Isn't the weather nice today?")
+
+# greet()
+
+
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"How do you do {name}")
+
+# greet_with_name('Billie')
+
+# def greet_with(name,location):
+#   print(f"Hello {name}")
+#   print(f"What is it like in {location}")
+
+# greet_with ('Hui', 'Melbourne')
+
+def keyword(name="hui", location="london"):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}")
+
+keyword()
